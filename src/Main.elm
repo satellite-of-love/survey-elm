80dcb240600c88976ba808756fe34afda924ae1b
Jessica Kerr <jessitron@gmail.com>
2017-04-16 10:48:30 -0500

    you can choose a kitty
    but why does it get huge

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Html exposing (Html)
 import Html.Attributes as Attr
+import Html.Events
 
 
 main : Program Never Model Msg
@@ -15,11 +16,17 @@ main =
 
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        Noop ->
+            model
+
+        Choose i ->
+            { model | chosen = Just i }
 
 
 type Msg
     = Noop
+    | Choose Int
 
 
 type alias Model =
@@ -28,7 +35,7 @@ type alias Model =
 
 model : Model
 model =
-    { options = kitties.options
+    { options = (List.sortBy .place kitties.options)
     , chosen = Nothing
     }
 
@@ -38,21 +45,37 @@ view model =
     Html.div
         []
         [ Html.div [ Attr.class "title" ] [ Html.text "Choose a Kitty" ]
-        , Html.div [ Attr.class "allKitties" ] [ Html.table [] [ Html.tr [] (List.map drawKitty model.options) ] ]
+        , Html.div
+            [ Attr.class "allKitties" ]
+            [ Html.table []
+                [ Html.tr []
+                    (List.map (drawKitty model.chosen) model.options)
+                ]
+            ]
         ]
 
 
-drawKitty : SurveyOption -> Html Msg
-drawKitty kitty =
-    Html.td
-        [ Attr.class "kitty"
-        , Attr.style [ ( "background-image", "url(" ++ kitty.imageLocation ++ ")" ) ]
-        ]
-        []
+drawKitty : Maybe Int -> SurveyOption -> Html Msg
+drawKitty chosen kitty =
+    let
+        itIsMe =
+            case chosen of
+                Just i ->
+                    kitty.place == i
+
+                _ ->
+                    False
+    in
+        Html.td
+            [ Html.Events.onClick (Choose kitty.place)
+            , Attr.classList [ ( "kitty", True ), ( "chosen", itIsMe ) ]
+            , Attr.style [ ( "background-image", "url(" ++ kitty.imageLocation ++ ")" ) ]
+            ]
+            []
 
 
 type alias SurveyOption =
-    { imageLocation : String, text : String }
+    { imageLocation : String, text : String, place : Int }
 
 
 type alias SurveyOptionsResponse =
@@ -66,12 +89,15 @@ kitties =
     , options =
         [ { imageLocation = "https://c1.staticflickr.com/4/3149/2988746750_4a3dfdee59.jpg"
           , text = "sink kitties"
+          , place = 1
           }
         , { imageLocation = "http://maxpixel.freegreatpicture.com/static/photo/1x/Sweet-Animals-Kitty-Cat-323262.jpg"
           , text = "kitty licking paw"
+          , place = 3
           }
         , { imageLocation = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Computer-kitten.jpg/1024px-Computer-kitten.jpg"
           , text = "computer kitten"
+          , place = 2
           }
         ]
     }

3fd3aac95609c9ae6f5aeebea66661fae3af4be4
Jessica Kerr <jessitron@gmail.com>
2017-04-16 09:15:32 -0500

    hello world

---
+++ src/Main.elm
@@ -0,0 +1,25 @@
+module Main exposing (main)
+
+import Html exposing (Html)
+
+
+main : Html never
+main =
+    Html.text "Hello World"
+
+
+kitties =
+    { seed = 123
+    , count = 3
+    , options =
+        [ { imageLocation = "https://c1.staticflickr.com/4/3149/2988746750_4a3dfdee59.jpg"
+          , text = "sink kitties"
+          }
+        , { imageLocation = "http://maxpixel.freegreatpicture.com/static/photo/1x/Sweet-Animals-Kitty-Cat-323262.jpg"
+          , text = "kitty licking paw"
+          }
+        , { imageLocation = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Computer-kitten.jpg/1024px-Computer-kitten.jpg"
+          , text = "computer kitten"
+          }
+        ]
+    }

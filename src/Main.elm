6168a75f706d084c354537d18ac7f8fd1a76822e
Jessica Kerr <jessitron@gmail.com>
2017-04-17 13:27:56 -0400

    set survey options to hypothetical service

--- src/Main.elm
+++ src/Main.elm
@@ -10,10 +10,12 @@ import SurveyResult exposing (SurveyResult, SurveyResultResponse)
 import VersionInfo exposing (versionInfo)
 
 
+surveyOptionsBaseUrl : String
 surveyOptionsBaseUrl =
-    "https://survey.atomist.com/survey-options"
+    "https://survey.atomist.com/woodstock"
 
 
+sendVoteBaseUrl : String
 sendVoteBaseUrl =
     "https://survey.atomist.com/survey-results"
 
@@ -227,6 +229,8 @@ findChoiceText options place =
 
 
 --- HTTP
+-- surveyOptionsBaseUrl =
+--     "https://survey.atomist.com/survey-options"
 
 
 type RemoteData e a
